95a134f68e1fbe4820ea6b7e1898b7a796a9263e
skoleosho97 <samson.koleosho@smoothstack.com>
2025-10-08 02:23:02 -0400

    Changed base configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/uma-career-planner/',
 	plugins: [react()],
+	base: '/uma-career-planner/'
 })
